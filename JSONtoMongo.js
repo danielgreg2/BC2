49ff570d7b9ad1154f20f74da7672f3c4c23a979
danielgreg2 <gdecanio@ufl.edu>
2019-09-16 19:21:36 -0400

    Meets all criteria

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -12,6 +12,7 @@ var fs = require('fs'),
 /* Connect to your database using mongoose - remember to keep your key secret*/
 //see https://mongoosejs.com/docs/connections.html
 //See https://docs.atlas.mongodb.com/driver-connection/
+mongoose.connect(config.db.uri);
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
@@ -20,6 +21,35 @@ var fs = require('fs'),
 
   Remember that we needed to read in a file like we did in Bootcamp Assignment #1.
  */
+ fs.readFile('listings.json', 'utf8', function(err, data) {
+	 if (err) {return console.log(err);}
+	 
+	 //parse listings.JSON object
+	 var parsedListings = JSON.parse(data);
+	 //console.log(parsedListings);		//DEBUGGING - shows the listings.json object 
+	 
+	 //iterate through parsed listings.JSON and make a model for each object
+	 parsedListings.entries.forEach(function(entry) {
+		 //console.log(entry.name);		//DEBUGGING - prints out each listing's name
+		 //console.log(entry.coordinates);		//DEBUGGING - prints out {latitude: , longitude: }
+		 
+		 //store listing info into a schema document
+		 var newListing = Listing({
+			 code: entry.code,
+			 name: entry.name,
+			 coordinates: entry.coordinates,
+			 address: entry.address
+		 });
+		 
+		 //save schema document to database collection
+		 newListing.save(function(err) {
+			 if (err) throw err;
+			 
+			 //console.log('Entry created!');		//DEBUGGING - states that a listing has been saved to the database
+		 });
+	 });
+	 console.log('\nLast entry added to database\n');
+ });
 
 
 /*  
--- queries.js
+++ queries.js
@@ -1,6 +1,11 @@
 /* Add all the required libraries*/
+var mongoose = require('mongoose'), 
+    Schema = mongoose.Schema, 
+    Listing = require('./ListingSchema.js'), 
+    config = require('./config');
 
 /* Connect to your database using mongoose - remember to keep your key secret*/
+mongoose.connect(config.db.uri);
 
 /* Fill out these functions using Mongoose queries*/
 //Check out - https://mongoosejs.com/docs/queries.html
@@ -10,6 +15,14 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+   
+	//find Lib West by name of entry
+	Listing.find({name: 'Library West'}, function(err, entry){
+	   if (err) throw err;
+	   
+	   //print Lib West's data to console
+	   console.log('\n' + entry + '\n');
+	});
 };
 var removeCable = function() {
   /*
@@ -17,6 +30,14 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+   
+    //find and remove 'CABL' entry by code (in one command)
+	Listing.findOneAndRemove({code: 'CABL'}, function(err, entry){
+		if (err) throw err;
+	  
+		//print success notification to console
+		console.log('\n' + entry.code + ' was successfully deleted.\n');
+	});
 };
 var updatePhelpsLab = function() {
   /*
@@ -26,14 +47,31 @@ var updatePhelpsLab = function() {
     Correct Address: 1953 Museum Rd, Gainesville, FL 32603
 
    */
+   
+	//find Phelps Lab by code, and update correct address
+	Listing.findOneAndUpdate({code: 'PHL'}, {address: '1953 Museum Rd, Gainesville, FL 32603'}, function(err, entry) {
+		if (err) throw err;
+		
+		//print success notification to console
+		console.log('\n' + entry.name + ' address was updated.\n');
+	});
+	
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+
+	//find all listings
+	Listing.find({}, function(err, entries) {
+		if (err) throw err;
+		
+		//print query of all entries to console
+		console.log(entries);
+	});
 };
 
 findLibraryWest();
 removeCable();
-updatePhelpsMemorial();
+updatePhelpsLab();
 retrieveAllListings();
